909b3f9375c4778611ac5dee889e7b291202e144
Drew Harry <drew.harry@gmail.com>
2012-05-27 18:03:04 -0400

    Show/hide completely working for photobooth with background clicking, too.

--- static/js/memoji.js
+++ static/js/memoji.js
@@ -4,6 +4,10 @@ var photoboothShown = false;
 $(document).ready(function() {
     console.log("Hello world!");
     
+    $("#photobooth").hide();
+    $("#background").hide();
+    
+    
     $(".emoji").click(function() {
         $(".emoji").removeClass("select");
         $(this).addClass("select");
@@ -17,7 +21,7 @@ $(document).ready(function() {
         showPhotobooth();
     });
     
-    $("#logo").click(function() {
+    $("#logo, #background").click(function() {
         if(photoboothShown) {
             hidePhotobooth();
         }
@@ -27,13 +31,23 @@ $(document).ready(function() {
 
 
 function showPhotobooth() {
-    $("#photobooth").animate({height: "400px"}, 500, "linear");
+    $("#photobooth").show();
+    $("#background").show().animate({opacity: "0.5"}, 500, "linear");
+
+    $("#photobooth").animate({height: "400px"}, 500, "linear", function() {
+        // ?
+    });
     
     photoboothShown = true;
 }
 
 function hidePhotobooth() {
-    $("#photobooth").animate({height: "0px"}, 500, "linear");
+    $("#background").animate({opacity: "0.0"}, 500, "linear", function() {
+        $(this).hide();
+    });
+    $("#photobooth").animate({height: "0px"}, 500, "linear", function() {
+        $("#photobooth").hide();
+    });
     
     photoboothShown = true;
 }
