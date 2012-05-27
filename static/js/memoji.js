fd24f63027390bec75bd88e7f42f95ec11ed1afb
Drew Harry <drew.harry@gmail.com>
2012-05-27 15:44:17 -0400

    Got tabs working on the main page.

--- static/js/memoji.js
+++ static/js/memoji.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
     console.log("Hello world!");
+    
+    
+    $(".emoji").click(function() {
+        $(".emoji").removeClass("select");
+        $(this).addClass("select");
+    });
+    
 });
