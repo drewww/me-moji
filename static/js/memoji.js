1e9eaa4d6f0102fe48d0a8fab4c5150a62abe983
Drew Harry <drew.harry@gmail.com>
2012-05-27 17:13:23 -0400

    Porting the camera code from camera.ejs into index.ejs and managing the transitions in and out of the camera mode.

--- static/js/memoji.js
+++ static/js/memoji.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
     console.log("Hello world!");
     
-    
     $(".emoji").click(function() {
         $(".emoji").removeClass("select");
         $(this).addClass("select");
