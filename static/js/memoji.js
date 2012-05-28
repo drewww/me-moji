14103c9dc23abb52461049a83e25be3ee7cdefd5
Drew Harry <drew.harry@gmail.com>
2012-05-27 20:43:13 -0400

    Copied in all the old camera logic and started hooking it up to the real graphics. Looking good.

--- static/js/memoji.js
+++ static/js/memoji.js
@@ -1,4 +1,12 @@
 
+var countdown = 2;
+
+var canvas;
+var pos = 0, ctx = null, image = [];
+var rowCount = 0;
+
+var emojiId = Math.floor(Math.random()*19);
+
 var photoboothShown = false;
 
 $(document).ready(function() {
@@ -27,6 +35,94 @@ $(document).ready(function() {
         }
     });
     
+    // From here down is all camera setup junk, mostly copied from camera.ejs.
+    $("#select").click(function() {
+		upload(image);
+		setMode("CAMERA");
+	});
+	
+	$("#reject").click(function() {
+		setMode("CAMERA");
+	});
+	
+	setMode("CAMERA");
+
+	$("#video").webcam({
+		width: 320,
+		height: 240,
+		mode: "callback",
+		swffile: "/static/js/lib/jscam/jscam.swf",
+		onTick: function() {
+			console.log("tick!");
+			highlight(countdown);
+			countdown--;
+		},
+		onSave: function(data) {
+			var col = data.split(";");
+			var img = image;
+			for(var i = 40; i < 280; i++) {
+				// first check and see if we're within the circle.
+				// if we aren't, set it to white + full alpha
+				
+				var x = i;
+				var y = rowCount;
+				var alpha = 0xff;
+				var distance = Math.sqrt(Math.pow(x-160, 2)+Math.pow(y-120, 2));
+				if(distance>120.5) {
+					alpha = 0x00;
+				} else if(distance>119.5) {
+					alpha = 0x80;
+				}
+				
+				var tmp = parseInt(col[i]);
+				img.data[pos + 0] = (tmp >> 16) & 0xff;
+				img.data[pos + 1] = (tmp >> 8) & 0xff;
+				img.data[pos + 2] = tmp & 0xff;
+				img.data[pos + 3] = alpha;
+				pos+= 4;
+			}
+
+			if (pos >= 4 * 240 * 240) {
+				ctx.putImageData(img, 0, 0);
+				pos = 0;
+				
+				// be done!
+				return;
+			}
+			
+			rowCount++;
+		},
+		onCapture: function() {
+			jQuery("#flash").css("display", "block");
+			jQuery("#flash").fadeOut("fast", function () {
+				jQuery("#flash").css("opacity", 1);
+			});
+			console.log("IN ON CAPTURE");
+			canvas = document.getElementById("image");
+			canvas.setAttribute('width', 240);
+			canvas.setAttribute('height', 240);
+			ctx = canvas.getContext("2d");
+			image = ctx.getImageData(0, 0, 240, 240);
+			rowCount = 0;
+			
+			setMode("REVIEW");
+			webcam.save();
+		},
+		debug: function(type, string) {console.log("debug: " + type + "; " + string);},
+		onLoad: function() {
+			console.log("load");
+		}
+	});
+
+	$("#capture").click(function() {
+		// show the countdown spans
+		setMode("COUNTDOWN");
+		window.webcam.capture(2);
+		highlight(3);
+		countdown = 2;
+	});
+    
+    
 });
 
 
@@ -50,4 +146,50 @@ function hidePhotobooth() {
     });
     
     photoboothShown = true;
+}
+
+
+function upload(image) {
+	$.post("/camera/", {"image": canvas.toDataURL("image/png"),
+	"emojiId":emojiId}, function(data) {
+		console.log("server response: " + data);
+	});
+}
+
+function highlight(num) {
+	$("#countdown span").css("opacity", 0.5);
+	$("#count" + num).css("opacity", 1.0);
+}
+
+function setMode(mode) {
+	console.log("SETTING MODE: "  + mode);
+	switch(mode) {
+		case "CAMERA":
+			// show #video
+			$("#image").hide();
+			$("#image-background").hide();
+			
+			
+			$("#camera").show();
+			$("#countdown").hide();
+			$("#review").hide();
+			break;
+		case "COUNTDOWN":
+			// show #video
+			$("#image").hide();
+			$("#image-background").hide();
+
+			$("#camera").hide();
+			$("#countdown").show();
+			$("#review").hide();
+			break;
+		case "REVIEW":
+			$("#image").show();
+			$("#image-background").show();
+
+			$("#camera").hide();
+			$("#countdown").hide();
+			$("#review").show();
+			break;
+	}
 }
