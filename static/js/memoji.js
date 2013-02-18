456c449c1e39c7622d7304594a71d004cf1d5906
Drew Harry <drew.harry@gmail.com>
2013-02-18 12:04:57 -0500

    Fixed #92

--- static/js/memoji.js
+++ static/js/memoji.js
@@ -69,6 +69,8 @@ $(document).ready(function() {
     
     $(".spinner").hide();
     
+    $("div.notice").hide();
+    
     // on load, check the sessionPhotos list to see if this session has
     // photos we should load in.
     for(var emojiIdIndex in sessionPhotos) {
